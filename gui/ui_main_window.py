6a3af0bd472c0e5c4540be9a6895c58234a1e452
AlgoTrader5 <AlgoTrader5@users.noreply.github.com>
2020-12-29 21:07:35 -0600

    "COINBASE-BTC-USD"

--- gui/ui_main_window.py
+++ gui/ui_main_window.py
@@ -2,11 +2,13 @@ import os
 import sys
 import argparse
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+from ui_market_window import MarketWindow
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config",   default="cryptogui/config.yaml", help='cryptostore configuration')
-parser.add_argument("--zmq-port", dest="zmq_port", default=5556,   help='cryptostore zmq connection')
+parser.add_argument("--config",   default="config.yaml", help='cryptostore configuration containing subscribed instruments')
+parser.add_argument("--zmq-port", dest="zmq_port", default=5556,   help='zmq connection to receive market updates')
 args = parser.parse_args()
 
 
@@ -21,41 +23,28 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setGeometry(50, 50, 600, 400)
         self.setWindowTitle('ConfigUI')
         self.setFont(QtGui.QFont("Helvetica [Cronyx]", 10))
-        self.init_menu()
         self.init_central_area()
 
-    def load_config(self, config):
-        pass
     
     def _tick_event_handler(self, t):
         self.market_window.tick_signal.emit(t)
 
-    def init_menu(self):
-        menubar = self.menuBar()
-        sysMenu = menubar.addMenu('File')
-        
-        # open folder
-        sys_folderAction = QtWidgets.QAction('Folder', self)
-        sys_folderAction.setStatusTip('Open_Folder')
-        sys_folderAction.triggered.connect(self.open_proj_folder)
-        sysMenu.addAction(sys_folderAction)
-        sysMenu.addSeparator()
-        
-        # sys|exit
-        sys_exitAction = QtWidgets.QAction('Exit', self)
-        sys_exitAction.setShortcut('Ctrl+Q')
-        sys_exitAction.setStatusTip('Exit_App')
-        sys_exitAction.triggered.connect(self.close)
-        sysMenu.addAction(sys_exitAction)
 
     def init_central_area(self):
+        self.central_widget = QtWidgets.QWidget()
+
         #-------------------------------- Top Left ------------------------------------------#
-        topleft = MarketWindow()
+        topleft = MarketWindow(subscriptions=self.config)
         self.market_window = topleft
         # -------------------------------- Top right ------------------------------------------#
-      
-        self.central_widget = QtWidgets.QWidget()
+        topright = QtWidgets.QWidget()
+        
+        splitter1 = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(topright)
+        splitter1.setSizes([700,450])
         hbox = QtWidgets.QHBoxLayout()
+        hbox.addWidget(splitter1)
         main_layout = QtWidgets.QHBoxLayout()
         win = QtWidgets.QWidget()
         win.setLayout(main_layout)
@@ -64,15 +53,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setCentralWidget(self.central_widget)
     
     def closeEvent(self, a0: QtGui.QCloseEvent):
+        # close any running processes/threads here
         print("QtGui.QCloseEvent")
 
         
 def main():
     app = QtWidgets.QApplication([])
-    import qdarkstyle
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-
-    win = MainWindow(args.config, args.zmq_port)
+    try:
+        import qdarkstyle
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    except ImportError:
+        e
+    # config = args.config
+    config = ["COINBASE-BTC-USD","COINBASE-ETH-USD","COINBASE-ETH-BTC"]
+    win = MainWindow(config, args.zmq_port)
     win.show()
-    win.showMaximized()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
